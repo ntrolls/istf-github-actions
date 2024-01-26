a71babaa8403d4564cc9c32499918723dfb8034e
ntrolls <shin.yoo@kaist.ac.kr>
2024-01-26 15:54:06 +0900

    updated test

--- test_triangle.py
+++ test_triangle.py
@@ -3,6 +3,12 @@ from triangle import triangle
 def test_invalid1():
     assert triangle(-1, 0, 1) == -1
 
+def test_normal():
+    assert triagle(3, 4, 2) == 1
+
+def test_isosceles():
+    assert triangle(2, 2, 1) == 2
+
 def test_equilateral():
-    assert triangle(3, 3, 3) == 1
+    assert triangle(3, 3, 3) == 3
 
